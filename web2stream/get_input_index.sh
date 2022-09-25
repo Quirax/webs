89c3aae6acf60d3293d8a523799731cf97143728
Quirax Lee <quiraxical@gmail.com>
2022-09-25 14:54:22 +0900

    ffmpeg tuning

--- web2stream/get_input_index.sh
+++ web2stream/get_input_index.sh
@@ -1,4 +1,4 @@
 # This script will get the index for the input based on the pid of the process
 # that initiated the sound in this case chrome
-pid=`ps -ef | grep $1 | grep AudioService | awk {'print $2'}`
+pid=`ps -ef | grep $1 | grep mojom.AudioService | awk {'print $2'}`
 pacmd list-sink-inputs | awk -v pid=$pid '/index:/ {idx=$2} /application.process.id/ {gsub(/"/, "", $3); if(int($3) == pid) print idx};'
